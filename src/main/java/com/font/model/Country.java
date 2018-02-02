7ca8c1db01c18de064d47f9222e5e999d4c8cea5
XiaoHang <1083594261@1-PC>
2018-02-02 15:51:53 +0800

    转换maven项目，为下面的ajax前端测试创建开发环境

---
+++ src/main/java/com/font/model/Country.java
@@ -0,0 +1,29 @@
+package com.font.model;
+/**
+ * 国家
+ * @author Administrator
+ *
+ */
+public class Country {
+	
+	private String name;
+	
+	private String code;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
